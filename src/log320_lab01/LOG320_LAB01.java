d4c3891097acda8d46734e6521a982d5ac2caee7
Zeldorine <cazorlatony@gmail.com>
2016-01-11 14:39:26 -0600

    fisrt commit

--- src/log320_lab01/LOG320_LAB01.java
+++ src/log320_lab01/LOG320_LAB01.java
@@ -15,7 +15,7 @@ public class LOG320_LAB01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic 
     }
     
 }
